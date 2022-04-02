4a8a37c3b57c2f3df8ab7f4d6bcaf781e3de103d
leo1p <leo1pacheco@hotmail.com>
2022-04-02 05:56:34 -0500

    first commit

---
+++ src/footer.js
@@ -0,0 +1,24 @@
+import './footer.css';
+import { DateTime } from 'luxon';
+const getGreetingTime = (d = DateTime.now()) => {
+    const split_afternoon = 12; // 24hr time to split the afternoon
+    const split_evening = 17; // 24hr time to split the evening
+    const currentHour = parseFloat(d.toFormat('hh'));
+
+    if (currentHour >= split_afternoon && currentHour <= split_evening) {
+        return 'afternoon';
+    } else if (currentHour >= split_evening) {
+        return 'evening';
+  }
+    return 'morning';
+}
+const Footer = ({ t }) => (
+  <div className="Footer">
+    <div>{t('footer.date', { date: new Date(), context: getGreetingTime()  })}</div>
+  </div>
+);
+
+export default Footer;
+
+// imported in our App.js and used like this
+// <Footer t={t} />
